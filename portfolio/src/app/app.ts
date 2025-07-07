03993eae1d2737f7c30c1e68ef856b9840cb27af
asfuller <alexfuller123@yahoo.com>
2025-07-07 17:04:32 -0400

    another initial commit

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
-import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
